9baaa10cd170a6558ac69317c1bda1610de5cc34
Dale Zhang <eslzzyl@163.com>
2023-04-20 09:33:17 +0000

    按照教程实现初步异步？

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 mod exception;
+mod param;
+mod config;
 
-use std::net::Ipv4Addr;
-use std::net::SocketAddrV4;
-use std::net::TcpListener;
-use std::net::TcpStream;
+use param::*;
+
+// use std::net::Ipv4Addr;
+// use std::net::SocketAddrV4;
+// use std::net::TcpListener;
+// use std::net::TcpStream;
 use std::io;
-use std::io::prelude::*;
 use std::fs;
 
-const CRLF: &str = "\r\n";
+use async_std::task::spawn;
+use async_std::prelude::*;
+use async_std::net::Ipv4Addr;
+use async_std::net::SocketAddrV4;
+use async_std::net::TcpListener;
+use async_std::net::TcpStream;
 
-fn main() -> io::Result<()> {
+use futures::stream::StreamExt;
+
+// const CRLF: &str = "\r\n";
+
+#[async_std::main]
+async fn main() -> io::Result<()> {
     // 监听端口
     let port: u16 = 7878;
     // 地址，本地调试用127.0.0.1
@@ -18,7 +31,7 @@ fn main() -> io::Result<()> {
     // 拼接socket
     let socket = SocketAddrV4::new(address, port);
 
-    let listener = match TcpListener::bind(socket) {
+    let listener = match TcpListener::bind(socket).await {
         Ok(listener) => listener,
         Err(e) => {
             println!("Failed to bind port: {}", port);
@@ -27,24 +40,26 @@ fn main() -> io::Result<()> {
     };
 
     // TcpListener::incoming函数返回迭代器，等价于无限循环地调用TcpListener::accept
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream);
-    }
+    listener
+        .incoming()
+        .for_each_concurrent(/* limit */ None, |tcpstream| async move {
+            let tcpstream = tcpstream.unwrap();
+            spawn(handle_connection(tcpstream));
+        }).await;
 
     Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).await.unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK", "html/index.html")
+        ("HTTP/1.1 200 OK", HTML_INDEX)
     } else {
-        ("HTTP/1.1 404 NOT FOUND", "html/404.html")
+        ("HTTP/1.1 404 NOT FOUND", HTML_404)
     };
 
     let contents = fs::read_to_string(filename).unwrap();
@@ -56,6 +71,6 @@ fn handle_connection(mut stream: TcpStream) {
         contents
     );
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    stream.write(response.as_bytes()).await.unwrap();
+    stream.flush().await.unwrap();
 }
---
+++ src/param.rs
@@ -0,0 +1,2 @@
+pub const HTML_INDEX: &str = r"files/html/index.html";
+pub const HTML_404: &str = r"files/html/404.html";
